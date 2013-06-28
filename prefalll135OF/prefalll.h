3b76dbba99eb80da1e9536abdf5aeffd2fda0012
peqpez <peqpez@gmail.com>
2013-06-28 15:44:23 +0300

    Update prefalll.h

--- prefalll135OF/prefalll.h
+++ prefalll135OF/prefalll.h
@@ -33,28 +33,21 @@ public:
 	void keyPressed  (int key);
 	void mouseMoved(int x, int y );
 	void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
 
 	void windowResized(int w, int h);
 
 	void fadeToColor(float r, float g, float b, float speed);
 	void addToFluid(int id, Vec2f pos, Vec2f vel, bool addColor, bool addForce);
 
-	int					fluidCellsX;
-	bool				resizeFluid;
-	bool				drawFluid;
-	bool				drawParticles;
+	int fluidCellsX;
+	bool resizeFluid;
+	bool drawFluid;
+	bool drawParticles;
 
-	MSA::FluidSolver	fluidSolver;
-	MSA::FluidDrawerGl	fluidDrawer;
-
-
-
-	MSA::Vec2f				pMouse;
-
-
-
-
+	MSA::FluidSolver fluidSolver;
+	MSA::FluidDrawerGl fluidDrawer;
+	MSA::Vec2f pMouse;
 
         Fish flock[NUM];
         Fish bigfish1;
@@ -63,23 +56,19 @@ public:
 
         //ripple
         float rippleX, rippleY;
-        bool hasRipple,rm;
+        bool hasRipple,rm, calibration;
         int hasPredator;
 
 
         Mill mills[MNUM];
 
         ofxOscSender sender;
-		ofArduino arduino;
+	ofArduino arduino;
 
-		int i,val0, status;
-		float val;
-		bool bSetupArduino;
+	int i,val0,status;
+	float val;
+	bool bSetupArduino;
         char* label[9];
-
-        bool calibra;
-
-
 
 
 };
